b90f27335a9f05ff94018990f9ee749a534ca243
Agencia Kivora <kivoraofc@gmail.com>
2025-10-28 20:54:26 -0300

    Update privecall.py

--- privecall.py
+++ privecall.py
@@ -6,15 +6,11 @@ app = Flask(__name__)
 
 @app.route("/criar_sala", methods=["GET"])
 def criar_sala():
-    # Seu link secreto (não mostre pra ninguém)
-    secret = "w1xr4b"
+    secret = "w1xr4b"  # sua chave do PriveCall
     url = f"https://privecall.com/api/automation/{secret}"
+    res = requests.post(url)
+    data = res.json()
 
-    # Faz o POST pra PriveCall criar a sala
-    response = requests.post(url)
-    data = response.json()
-
-    # Se der certo, redireciona o cliente direto pra sala
     if data.get("success"):
         return redirect(data["chatUrl"])
     else:
